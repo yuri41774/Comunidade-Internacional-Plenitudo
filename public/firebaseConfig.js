fd0c7340c3eab2a526d2d8a48b7a8eb3613e7901
yuri41774 <yuri41774@gmail.com>
2025-10-01 23:46:52 +0000

    Deploy: atualização para produção

---
+++ public/firebaseConfig.js
@@ -0,0 +1,10 @@
+// NÃO SUBA ESTE ARQUIVO PARA REPOSITÓRIOS PÚBLICOS
+export const firebaseConfig = {
+  apiKey: "AIzaSyAxuEEDjn6lPfKOysOcQjvWvUWqJL_990o",
+  authDomain: "c-i-plenitude.firebaseapp.com",
+  projectId: "c-i-plenitude",
+  storageBucket: "c-i-plenitude.appspot.com",
+  messagingSenderId: "126733729315",
+  appId: "1:126733729315:web:9f247cd8f854fb2e0fbb96",
+  measurementId: "G-CGG5MZB9P1"
+};
